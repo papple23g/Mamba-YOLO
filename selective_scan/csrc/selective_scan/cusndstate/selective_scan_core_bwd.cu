9c37bad8fbde79b9c0187de3bebdf2b67600b046
papple23g <papple12g@gmail.com>
2024-08-18 07:26:56 +0800

    fix: 🐛修改 `selective_scan` 目錄內的代碼。解決 `pip install .` 報錯問題
    https://blog.csdn.net/yyywxk/article/details/140422758#:~:text=2-,Windows%20%E4%B8%8B%20selective_scan%20%E7%9A%84%E7%BC%96%E8%AF%91,-%E5%B0%86kernels/selective_scan

--- selective_scan/csrc/selective_scan/cusndstate/selective_scan_core_bwd.cu
+++ selective_scan/csrc/selective_scan/cusndstate/selective_scan_core_bwd.cu
@@ -1,6 +1,10 @@
 /******************************************************************************
  * Copyright (c) 2023, Tri Dao.
  ******************************************************************************/
+#ifndef M_LOG2E
+#define M_LOG2E 1.4426950408889634074
+#endif
+
 #include "selective_scan_bwd_kernel_ndstate.cuh"
 
 template void selective_scan_bwd_cuda<1, float, float>(SSMParamsBwd &params, cudaStream_t stream);
